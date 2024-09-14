b6357556f00386c50069000fffd1c2db40eac399
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-09-14 14:51:26 +0530

    minor issues

--- app/(content)/tasks/page.tsx
+++ app/(content)/tasks/page.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import { useState } from "react";
-
 export default function page() {
-
-  const [search, setSearch] = useState("");
 
   return (
     <div className="flex flex-col px-8 py-10 gap-10">
@@ -20,8 +16,6 @@ export default function page() {
           name="search"
           placeholder="Search task here..."
           className="border-2 border-purple-950 rounded-lg px-4 py-2 text-lg"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
         />
       </div>
       <ul>
